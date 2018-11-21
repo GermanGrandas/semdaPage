c4e5852c5c75615c2060518903953ec5916ef05c
GermanGrandas <gertiven@gmail.com>
2018-11-20 23:44:14 -0500

    Updates

---
+++ precache-manifest.3531fcb2e42bc7016b4fdd43c7ce6384.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/semdaPage/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "b8117ae3055bb1476907",
+    "url": "/semdaPage/static/js/runtime~main.b8117ae3.js"
+  },
+  {
+    "revision": "2a4f5023e75080973ecd",
+    "url": "/semdaPage/static/js/main.2a4f5023.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/semdaPage/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "2a4f5023e75080973ecd",
+    "url": "/semdaPage/static/css/main.9744180c.chunk.css"
+  },
+  {
+    "revision": "73ef6a6367c5a84e632c8c89dad7f20a",
+    "url": "/semdaPage/index.html"
+  }
+];
